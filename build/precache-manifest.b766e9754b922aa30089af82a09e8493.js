88aba2243bbace28b255b9bfecbade492b44024e
Florin Pop <popflorin1705@yahoo.com>
2020-01-20 18:24:07 +0200

    init

---
+++ build/precache-manifest.b766e9754b922aa30089af82a09e8493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03913825b8a5d09f1c36a1c154cb8f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c02dda796a0b23b6357a",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "fcd0d6e747df6d8f5f59",
+    "url": "/static/js/2.a2abfbb9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a2abfbb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c02dda796a0b23b6357a",
+    "url": "/static/js/main.e7441c71.chunk.js"
+  },
+  {
+    "revision": "e6ca1d3bd6c7904ad7f8",
+    "url": "/static/js/runtime-main.413705cc.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import './App.css';
+
+// TODO:
+// MARKUP
+// box / container ✅
+// input + button ✅
+// list of items ✅
+// button ✅
+
+// FUNCTIONALITIES
+// input -> save to state ✅
+// button (top) -> add to list ✅
+// button (bottom) -> trigger the randomizer ✅
+
+const defaultItems = [
+	{
+		text: 'Option 1',
+		selected: false
+	},
+	{
+		text: 'Option 2',
+		selected: false
+	},
+	{
+		text: 'Option 3',
+		selected: false
+	},
+	{
+		text: 'Option 4',
+		selected: true
+	}
+];
+
+function App() {
+	const [items, setItems] = useState(defaultItems);
+	const [inputValue, setInputValue] = useState('');
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		const newItem = {
+			text: inputValue,
+			selected: false
+		};
+
+		const newItems = [...items, newItem];
+
+		setInputValue('');
+		setItems(newItems);
+	};
+
+	const randomizer = () => {
+		for (let i = 0; i < 20; i++) {
+			setTimeout(pickRandomItem, 100 * i + 100);
+		}
+	};
+
+	const pickRandomItem = () => {
+		const randomItem = items[Math.floor(Math.random() * items.length)];
+
+		const newItems = items.map(item =>
+			item === randomItem
+				? { ...item, selected: true }
+				: { ...item, selected: false }
+		);
+
+		setItems(newItems);
+	};
+
+	return (
+		<div className='container bg-white mx-auto shadow-lg w-2/4 h-auto p-6'>
+			<form onSubmit={handleSubmit} className='flex'>
+				<input
+					className='py-2 px-4 border border-gray-500 flex-1'
+					type='text'
+					placeholder='Add a new item'
+					value={inputValue}
+					onChange={e => {
+						setInputValue(e.target.value);
+					}}
+				/>
+				<button className='bg-blue-500 border border-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4'>
+					Add
+				</button>
+			</form>
+
+			<ul>
+				{items.map((item, idx) => (
+					<li
+						className={`my-5 p-2 ${
+							item.selected ? 'bg-orange-500 text-white' : ''
+						}`}
+						key={idx}>
+						{item.text}
+					</li>
+				))}
+			</ul>
+
+			{items.length > 0 && (
+				<button
+					className='bg-blue-500 w-full hover:bg-blue-700 text-white py-2 px-4'
+					onClick={randomizer}>
+					Randomize
+				</button>
+			)}
+		</div>
+	);
+}
+
+export default App;
